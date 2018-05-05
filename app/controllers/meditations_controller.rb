f5df7e36402e623887c8a71588d2fc3dcf186e63
viniciuspalma <pockvini@gmail.com>
2018-05-05 01:26:18 -0300

    create a endpoint to meditation session

--- app/controllers/meditations_controller.rb
+++ app/controllers/meditations_controller.rb
@@ -2,15 +2,39 @@ class MeditationsController < ApplicationController
   def index
     meditations = Meditation.includes(:user).public_sessions
 
-    json_response(MeditationSerializer.new(
-      meditations, { meta: { total: meditations.size }, include: [:user] }
-    ).serializable_hash)
+    json_response(
+      MeditationSerializer
+        .new(meditations, {
+          meta: {
+            total: meditations.size
+          },
+          include: [:user]
+        })
+        .serializable_hash
+      )
+  end
+
+  def show
+    json_response(
+      MeditationSerializer
+        .new(meditation)
+        .serializable_hash
+    )
   end
 
   private
 
+  def meditation
+    Meditation
+      .includes(:user)
+      .find(
+        params[:id] || meditation_params[:id]
+      )
+  end
+
   def meditation_params
     params.permit(
+      :id,
       :title,
       :audio,
       :start,
--- spec/requests/meditations_spec.rb
+++ spec/requests/meditations_spec.rb
@@ -16,4 +16,20 @@ RSpec.describe 'Meditations API', type: :request do
       expect(json['meta']['total']).to eq(2)
     end
   end
+
+  describe 'GET /meditations/:id' do
+    before {
+      get "/meditations/#{meditations[0].id}",
+      params: {},
+      headers: headers
+    }
+
+    it 'returns an ok status' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'returns an correct total' do
+      expect(json['data']['id']).to eq(meditations[0].id)
+    end
+  end
 end
